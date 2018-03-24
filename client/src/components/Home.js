61d8eb10eca24ec5e89bf011d568395b87aae4f4
Usong Cho <silvermouse4012@gmail.com>
2018-03-24 11:45:10 -0600

    commit1

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Footer } from 'semantic-ui-react';
 
 class Home extends Component {
   render() {
     return (
       <Header as='h1' textAlign='center'>B&D BURGER</Header>
+
     );
+    <footer as='h4' textAlign= 'center'>by the RAYBUL team</footer>
   }
 }
+
+
 
 export default Home;
